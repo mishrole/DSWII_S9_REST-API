b80e5eaa31a522a83ea04b2842cab4eb367fbd81
Mitchell Rodríguez <mitrodle@gmail.com>
2021-05-23 11:08:59 -0500

    Initial commit

---
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -0,0 +1,39 @@
+package com.empresa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.empresa.entity.Alumno;
+import com.empresa.service.AlumnoService;
+
+import lombok.extern.apachecommons.CommonsLog;
+
+@CommonsLog
+@RestController
+@RequestMapping("/rest/alumno")
+public class AlumnoController {
+	
+	@Autowired
+	private AlumnoService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Alumno>> lista() {
+		System.out.println(">>> inicio >>> lista");
+		List<Alumno> listAlumno = service.listaAlumno();
+		return ResponseEntity.ok(listAlumno);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Alumno> registra(@RequestBody Alumno obj) {
+		System.out.println(">>> inicio >>> registra \" + obj.getNombre()");
+		Alumno objAlumno = service.insertaActualizaAlumno(obj);
+		return ResponseEntity.ok(objAlumno);
+	}
+}
